84a25adc64a998950b30630b4705d12dfc9553ca
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2024-09-16 08:27:30 -0300

    home pronta

--- app/src/main/java/com/example/personagens2/HomeActivity.java
+++ app/src/main/java/com/example/personagens2/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.personagens2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Button buttonHistoria, buttonAutor, buttonPersonagens, buttonTimeline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,39 @@ public class HomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        buttonHistoria = findViewById(R.id.button);
+        buttonAutor = findViewById(R.id.button2);
+        buttonPersonagens = findViewById(R.id.button3);
+        buttonTimeline = findViewById(R.id.button4);
+
+        buttonHistoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, novaPagina.class);
+                startActivity(intent);
+            }
+        });
+        buttonAutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, novaPagina.class);
+                startActivity(intent);
+            }
+        });
+        buttonPersonagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, novaPagina.class);
+                startActivity(intent);
+            }
+        });
+        buttonTimeline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, novaPagina.class);
+                startActivity(intent);
+            }
         });
     }
 }
