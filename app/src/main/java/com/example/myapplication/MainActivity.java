48f63c70f50d095b856920acfaeaa3105dd46d39
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2024-09-23 09:10:09 -0300

    upd autor

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonHistoria, buttonAutor, buttonPersonagens, buttonTimeline;
+
+    private ViewPager2 viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        viewPager = findViewById(R.id.viewPager);
+
+        List<Integer> images = Arrays.asList(
+                R.drawable.avesso, // Adicione suas imagens aqui
+                R.drawable.beio,
+                R.drawable.darkside,
+                R.drawable.capadeus
+        );
+
+        MyPagerAdapter adapter = new MyPagerAdapter(images);
+        viewPager.setAdapter(adapter);
     }
 }
+
+
